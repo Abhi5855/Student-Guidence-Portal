dea594a2f5821b57152fab469c245e2c7065fab7
Abhi5855 <abhi95735@gmail.com>
2020-06-19 12:19:02 +0530

    test

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
-  {path="",component:HomeComponent},
-  {path="about",component:AboutComponent},
+  {path:"",component:HomeComponent},
+  {path:"about",component:AboutComponent},
 ];
 
 @NgModule({
