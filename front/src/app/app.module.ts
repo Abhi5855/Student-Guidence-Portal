8302da8ff73a39bc8f1a0162a94bb64cc85d590e
NAGAR <sanjayrathore144@gmail.com>
2020-06-22 13:49:00 +0530

    imported formsModule and HttpClientModule in app module.ts by sanjay rathore

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -7,18 +7,20 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-<<<<<<< HEAD
+
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { CardComponent } from './card/card.component';
-import { HttpClient} from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { LogoutComponent } from './logout/logout.component';
-=======
+
 import { GalleryComponent } from './gallery/gallery.component';
 import { HelpComponent } from './help/help.component';
->>>>>>> 065ded3bb91911d37263b3953cdc4a5d9eb84525
+
+
 
 @NgModule({
   declarations: [
@@ -27,22 +29,22 @@ import { HelpComponent } from './help/help.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-<<<<<<< HEAD
+
     SignInComponent,
     SignUpComponent,
     CardComponent,
     DashboardComponent,
     MainpageComponent,
-    LogoutComponent
-=======
+    LogoutComponent,
     GalleryComponent,
     HelpComponent
->>>>>>> 065ded3bb91911d37263b3953cdc4a5d9eb84525
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClient
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
