2ddd5da4ee762381c4aeb3637f0dc31438fcc8a5
Abhi5855 <abhi95735@gmail.com>
2020-06-22 14:21:04 +0530

    pull project that changed by sir

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { HelpComponent } from './help/help.component';
-import {ContactComponent} from './contact/contact.component'
-import {SignInComponent} from './sign-in/sign-in.component';
-import {SignUpComponent} from './sign-up/sign-up.component';
+import { ContactComponent } from './contact/contact.component'
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './auth.guard';
 import { MainpageComponent } from './mainpage/mainpage.component';
@@ -14,34 +14,35 @@ import { LogoutComponent } from './logout/logout.component';
 
 
 const routes: Routes = [
-<<<<<<< HEAD
-  {path:"", canActivate:[AuthGuard] ,component:MainpageComponent,children:[
-                                              {path:"",component:HomeComponent},
+  {
+    path: "", canActivate: [AuthGuard], component: MainpageComponent, children: [
+      { path: "", component: HomeComponent },
 
-                                              {path:"about",component:AboutComponent},
+      { path: "about", component: AboutComponent },
 
-                                              {path:"contact" , component:ContactComponent},
+      { path: "contact", component: ContactComponent },
 
-                                              {path:"sign-up",component:SignUpComponent},
+      { path: "sign-up", component: SignUpComponent },
 
-                                              {path:"sign-in",component:SignInComponent},
+      { path: "sign-in", component: SignInComponent },
 
-  ]},
-  {path:"dashboard" ,canActivate:[AuthGuard],component:DashboardComponent ,children:[
-                                              {
-                                                path:"logout", component:LogoutComponent
-                                              }
-  ]}
-=======
-  {path:"",component:HomeComponent},
-  {path:"about",component:AboutComponent},
-  {path:"gallery",component:GalleryComponent},
-  {path:"contact",component:ContactComponent},
-  {path:"help",component:HelpComponent},
-  {path:"contact" , component:ContactComponent},
-  {path:"sign-up",component:SignUpComponent},
-  {path:"sign-in",component:SignInComponent}
->>>>>>> 065ded3bb91911d37263b3953cdc4a5d9eb84525
+    ]
+  },
+  {
+    path: "dashboard", canActivate: [AuthGuard], component: DashboardComponent, children: [
+      {
+        path: "logout", component: LogoutComponent
+      }
+    ]
+  },
+  { path: "", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "gallery", component: GalleryComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "help", component: HelpComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "sign-up", component: SignUpComponent },
+  { path: "sign-in", component: SignInComponent }
 ];
 
 @NgModule({
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,24 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-<<<<<<< HEAD
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { CardComponent } from './card/card.component';
-import { HttpClient} from '@angular/common/http';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MainpageComponent } from './mainpage/mainpage.component';
-import { LogoutComponent } from './logout/logout.component';
-=======
-import { GalleryComponent } from './gallery/gallery.component';
-import { HelpComponent } from './help/help.component';
->>>>>>> 065ded3bb91911d37263b3953cdc4a5d9eb84525
+import{ HttpClient } from '@angular/common/http';
+import{ FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,22 +16,12 @@ import { HelpComponent } from './help/help.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-<<<<<<< HEAD
-    SignInComponent,
-    SignUpComponent,
-    CardComponent,
-    DashboardComponent,
-    MainpageComponent,
-    LogoutComponent
-=======
-    GalleryComponent,
-    HelpComponent
->>>>>>> 065ded3bb91911d37263b3953cdc4a5d9eb84525
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClient
+    HttpClient,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
